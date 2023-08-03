d84032e0c4c7e53f8cc5360824d7d4733bfe0c43
Ini-0luwa <lawalsonoluwasegun@gmail.com>
2023-08-03 00:39:39 -0700

    updated login page

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ import Login from "./pages/onboarding/Login";
 import { Box } from "@chakra-ui/react";
 import AdminRoute from "./routes/AdminRoute";
 import Registration from "./pages/onboarding/register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div>
       <Router>
-        
+        <ToastContainer />
         <Box>
           <Routes>
-           
             <Route path='/' element={<Login />} />
             <Route path='register' element={<Registration />} />
             <Route path='admin/*' element={<AdminRoute />} />
--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,25 @@
+import axios from "axios";
+
 const url = "http://localhost/child_abuse_management_system_db";
-
+const baseUrl =
+  "http://localhost/child_protection_information_management_system_db";
 export const api = async (page, data) => {
-    const request = await fetch(`${url}/${page}`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify(data)
-    });
-    
+  const request = await fetch(`${url}/${page}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
-    const res = await request.json();
-    console.log("response",res)
-    return res;
+  const res = await request.json();
+  console.log("response", res);
+  return res;
+};
 
-}
+export const postData = async (url, data) => {
+  let link = `${baseUrl}/${url}`;
+  const request = await axios.post(link, data);
+  return request;
+};
